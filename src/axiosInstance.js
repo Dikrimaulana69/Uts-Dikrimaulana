1fb3801a49f4e53e4a7368fe50d1a79669f53e96
Dikri Maulana <maulanadikri710@gmail.com>
2024-11-13 19:46:37 +0700

    cmmit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.161.210:3000', // Ubah dengan IPv4 komputer kamu
+  baseURL: 'http://192.168.118.58:3000', // Ubah dengan IPv4 komputer kamu
   headers: {
     'Content-Type': 'application/json',
   },
